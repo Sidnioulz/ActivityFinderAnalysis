48af0d7893360574c5ef3658ff4b386460d1c401
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2016-10-12 17:14:28 +0200

    A script to find symlinks among desktop files that are relative rather than absolute

---
+++ find-wrong-symlink.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#ln -s /location/to/link linkname
+#ln -s /location/to/link2 newlink
+#mv -T newlink linkname
+
+cd ../applications/usr/share/applications/
+
+for i in `find . -type l -ls | tr -s " " | cut -d" " -f 12`; do
+  dest=`ls $i -la`
+  if [ "x`echo $dest | grep $PWD`" = "x" ]; then
+    name="`echo $dest | cut -d" " -f9`"
+    target="`echo $dest | cut -d" " -f11`"
+#    echo $dest
+#    echo $name
+#    echo "$PWD/$target"
+    rm $name
+    ln -s "$PWD/$target" $name
+  fi
+done
+
+cd -
