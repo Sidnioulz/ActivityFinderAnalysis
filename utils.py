faa105138ba3f09eb37a38fa33e081aab5e8f50b
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2016-10-20 02:38:26 +0200

    URL to Unix path method, and unquote method

--- utils.py
+++ utils.py
@@ -1,10 +1,22 @@
 from datetime import datetime
+import os
+from urllib.parse import urlparse, unquote
 
 
 def timestampZgPrint(timestamp):
-    """ Transforms a Zeitgeist timestamp with milliseconds into a
-    human-readable string. """
+    """Transforms a Zeitgeist timestamp into a human-readable string."""
     (timestamp, remainder) = divmod(timestamp, 1000)
     string = datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S")
     string += (".%d" % remainder)
     return string
+
+
+def urlToUnixPath(url):
+    """Convert a file:// URL to a Unix path string."""
+    p = urlparse(url)
+    return unquote(os.path.abspath(os.path.join(p.netloc, p.path)))
+
+
+def uq(s):
+    """Unquote a string."""
+    return unquote(s)
