89ce6510024e3ea044d366d87512794c1c2269dd
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2016-10-20 02:39:22 +0200

    Introduced file flags for Event, to record when an event is by designation, etc. and abstract a little from syscalls/zg Events can now parse very basic ZG FileCreate/FileModify/FileAccess events, and save the file names and mime types

--- Event.py
+++ Event.py
@@ -1,9 +1,13 @@
 """An action performed by an Application at a specific time."""
 
 from enum import Enum
+from flags import Flags
 from Application import Application
 # from utils import timestampZgPrint
-# from constants import EV_TIMESTAMP, EV_INTERPRETATION_URI
+from constants import *  # EV_TIMESTAMP, EV_INTERPRETATION_URI
+from SqlEvent import SqlEvent, SqlEventSubject
+from File import File
+from utils import urlToUnixPath
 
 
 class EventSource(Enum):
@@ -17,6 +21,8 @@ class EventSource(Enum):
 
 class EventType(Enum):
     """Supported types of events, from Zeitgeist or PreloadLogger."""
+
+# create, destroy, read, write, designation, programmatic,
 
     unknown = 0
 
@@ -33,6 +39,18 @@ class EventType(Enum):
     appstart = 10
 
 
+class EventFileFlags(Flags):
+    """Flags for accesses to files in Events."""
+
+    create = 1 << 0
+    destroy = 1 << 1
+    displace = 1 << 2
+    read = 1 << 3
+    write = 1 << 4
+    designation = 1 << 5
+    programmatic = 1 << 6
+
+
 class Event(object):
     """An action performed by an Application at a specific time.
 
@@ -43,9 +61,11 @@ class Event(object):
 
     time = 0       # type: int; when the event occurred
     evtype = None  # type: EventType; type of the event
+    evflags = EventFileFlags.no_flags  # type: EventFileFlags
     actor = None   # type: list; actor responsible for performing the event
     source = EventSource.unknown  # type: EventSource; source of the event
     subjects = []  # type: list; other entities affected by the event
+    data = None    # binary data specific to each Event. Read the code...
 
     def __init__(self,
                  actor: Application,
@@ -65,6 +85,10 @@ class Event(object):
         self.actor = actor
         self.time = time
 
+        self.type = EventType.unknown
+        self.evflags = EventFileFlags.no_flags
+        self.data = []
+
         # Verify there's only one source
         if not zgEvent and not syscallStr and not cmdlineStr:
             raise ValueError("Events cannot be empty: please provide a log "
@@ -80,10 +104,7 @@ class Event(object):
 
         if zgEvent:
             self.source = EventSource.zeitgeist
-            # TODO Parse event! woop woop
-            # print("NEW EVENT: AT %s -- %s from ZG" % (
-            #        timestampZgPrint(self.time), zgEvent[EV_INTERPRETATION_URI])
-            #       )
+            self.parseZeitgeist(zgEvent)
 
         elif syscallStr:
             self.source = EventSource.preloadlogger
@@ -107,10 +128,63 @@ class Event(object):
             #        timestampZgPrint(self.time), syscall)
             #       )
 
+    def setDataFiles(self, zge: SqlEvent):
+        """Set data to a list of files (for simple file events)."""
+        self.data = []
+        for subj in zge.subjects:
+            if subj.uri.startswith("file://"):
+                f = File(path=urlToUnixPath(subj.uri),
+                         ftype=subj.mimetype)
+                self.data.append(f)
+
+    def setDataFilesDual(self, zge: SqlEvent):
+        """Set data to a list of file couples (for copy/move events)."""
+        # TODO add tuples (prev, next) for copies and moves
+
+    def parseZeitgeist(self, zge: SqlEvent):
+        """Process a Zeitgeist event to initialise this Event."""
+
+        if zge.interpretation in (
+             'activity://gui-toolkit/gtk2/FileChooser/FileCreate',
+             'activity://gui-toolkit/gtk3/FileChooser/FileCreate'):
+            self.evtype = EventType.filecreate
+            self.evflags |= EventFileFlags.create
+            self.evflags |= EventFileFlags.write
+            self.evflags |= EventFileFlags.designation
+            self.setDataFiles(zge)
+
+        if zge.interpretation in (
+             'activity://gui-toolkit/gtk2/FileChooser/FileAccess',
+             'activity://gui-toolkit/gtk3/FileChooser/FileAccess'):
+            self.evtype = EventType.fileread
+            self.evflags |= EventFileFlags.read
+            self.evflags |= EventFileFlags.designation
+            self.setDataFiles(zge)
+
+        if zge.interpretation in (
+             'activity://gui-toolkit/gtk2/FileChooser/FileModify',
+             'activity://gui-toolkit/gtk3/FileChooser/FileModify'):
+            self.evtype = EventType.filewrite
+            self.evflags |= EventFileFlags.write
+            self.evflags |= EventFileFlags.designation
+            self.setDataFiles(zge)
+
     def getTime(self):
         """Return the Event's time of occurrence."""
         return self.time
 
+    def getType(self):
+        """Return the Event's type."""
+        return self.evtype
+
+    def getFileFlags(self):
+        """Return the Event's flags related to files."""
+        return self.evflags
+
     def getActor(self):
         """Return the Application that performed this Event."""
         return self.actor
+
+    def getData(self):
+        """Return the Event's custom data."""
+        return self.data
