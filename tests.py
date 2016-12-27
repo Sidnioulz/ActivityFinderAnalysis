6b992b6a0bfc704fdb7c2a16efa6e0c943c13200
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2016-12-27 02:38:02 +0100

    Adding a basic test for app types (userland vs system vs desktop)

--- tests.py
+++ tests.py
@@ -352,6 +352,19 @@ class TestEventFlags(unittest.TestCase):
         FileStore.reset()
 
 
+class TestApplication(unittest.TestCase):
+    def test_app_types(self):
+        ff = Application("firefox.desktop", pid=1, tstart=0, tend=2)
+        cf = Application("catfish.desktop", pid=2, tstart=21, tend=32)
+        th = Application("thunar.desktop", pid=3, tstart=5, tend=8)
+        gd = Application("gnome-disks.desktop", pid=4, tstart=2, tend=4)
+
+        self.assertTrue(ff.isUserlandApp())
+        self.assertTrue(cf.isDesktopApp())
+        self.assertTrue(th.isDesktopApp())
+        self.assertTrue(gd.isDesktopApp())
+
+
 class TestApplicationStore(unittest.TestCase):
     def setUp(self):
         self.store = ApplicationStore.get()
@@ -368,7 +381,7 @@ class TestApplicationStore(unittest.TestCase):
         self.store.insert(f)
         self.assertEqual(len(self.store.lookupPid(21)), 3)
 
-    def test_merge_equal(self):
+    def test_merge_equal_b(self):
         self.store.clear()
         a = Application("firefox.desktop", pid=18495, tstart=0, tend=2)
         b = Application("firefox.desktop", pid=245, tstart=21, tend=32)
