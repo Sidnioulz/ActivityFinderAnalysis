ce9e2b3198d1afe583e41bf5ec6636d1ddab62c7
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2017-05-03 10:09:40 +0200

    Frequent file engine now analyses relations between files in found patterns and saves output to file.

--- FrequentFileEngine.py
+++ FrequentFileEngine.py
@@ -1,6 +1,8 @@
 """An engine to mine patterns of frequently co-accessed Files."""
+from File import File
 from utils import outputFsEnabled, tprnt, frequency
 import mimetypes
+import itertools
 
 
 class FrequentFileEngine(object):
@@ -63,12 +65,6 @@ class FrequentFileEngine(object):
 
         # Write the other input file, for aggregation via Python API.
         with open(self.outputDir + '/' + 'typesPerInstance.list', 'w') as f:
-            # TODO add all folder elements to find folders that contain more of a file type
-            # TODO       itemsets with two types are "co-frequent types".
-            # TODO       itemsets without types must be searched for mutually exclusive folders (not parents of one another).
-            # TODO then, itemsets with one type + folders are frequent types for those folders.
-            # TODO       itemsets with just one folder are frequent folders.
-            # TODO       itemsets with just one type are frequent types.
             for app in apps:
                 msg = '%s\t' % (app.uid())
                 l = accessedTypesPerApp.get(app) or []
@@ -215,13 +211,24 @@ class FrequentFileEngine(object):
         #             disp.add(elem)
         #     displayPatterns[p] = disp
 
-        tprnt("\nMost commonly found types:")
-        for item in sorted(uniques, key=lambda x: x[1], reverse=True):
-            print("\t", item)
+        # Print to files.
+        with open(self.outputDir + '/' + 'patterns.out', 'w') as f:
+            tprnt("\nMost commonly found types:")
+            print("Most commonly found types:", file=f)
+            for item in sorted(uniques,
+                               key=lambda x: x[1],
+                               reverse=True):
+                print("\t", item)
+                print("mcft\t", item, file=f)
 
-        tprnt("\nMost commonly found patterns:")
-        for item in sorted(patterns.items(), key=lambda x: x[1], reverse=True):
-            print("\t", item)
+            tprnt("\nMost commonly found patterns:")
+            print("\nMost commonly found patterns:", file=f)
+            for item in sorted(patterns.items(),
+                               key=lambda x: x[1],
+                               reverse=True):
+                print("\t", item)
+                print("mcfp\t", item, file=f)
+            print("", file=f)
 
         del itemsets
 
@@ -236,17 +243,147 @@ class FrequentFileEngine(object):
                     matches.append(t)
                     transactionsPerPattern[p] = matches
         tprnt("Done.")
-            
-        tprnt("\nPrinting matched transactions...")
-        line = ""
-        # Print all the transactions that match a pattern, for manual analysis.
-        for (p, matches) in sorted(transactionsPerPattern.items()):
-            tprnt("%s%d\tPATTERN: %s" % (line, patterns[p], p.__str__()))
-            line = "\n"
 
+        def _printPattern(p, matches, counter, exclusiveCounter):
+            msg = ""
+            listing = ""
+            summary = ""
+
+            # Base pattern identity.
+            msg += ("\n\nPATTERN: %d\t%s" % (patterns[p], p.__str__()))
+
+            # Transaction listing.
             for matchedTransaction in matches:
-                print("\tApp: %s" % matchedTransaction[0])
+                listing += ("App: %s\n" % matchedTransaction[0])
                 for transactionElem in sorted(matchedTransaction[1:]):
-                    print("\t* %s" % transactionElem)
-                print("")
+                    listing += ("\t* %s\n" % transactionElem)
+                listing += ("\n")
+
+            # Counters of file extension co-occurrences.
+            for (k, v) in sorted(counter.items()):
+                summary += ("\t{%s} occurs %d times, in %d patterns\n" % (
+                            ','.join(k), v, counterI[k]))
+            summary += "\n"
+            for (k, v) in sorted(exclusiveCounter.items()):
+                summary += ("\t{%s} is exclusive %d times, in %d patterns\n" % (
+                            ','.join(k), v, exclusiveCounterI[k]))
+
+            # Print to files.
+            with open(self.outputDir + '/' + 'patterns.out', 'a') as f:
+                print(msg, file=f)
+                print(summary, file=f)
+
+            with open(self.outputDir + '/' + 'patternsListing.out', 'a') as f:
+                print(msg, file=f)
+                print(listing, file=f)
+
+        # Pre-analyse the relationships between file endings in patterns.
+        tprnt("\nPre-analysing the relationships between files in patterns...")
+        for (p, matches) in sorted(transactionsPerPattern.items()):
+            # Counter used to count combos of files with the same name and
+            # different extensions.
+            counter = dict()
+            exclusiveCounter = dict()
+            counterI = dict()
+            exclusiveCounterI = dict()
+
+            # Go through file accesses that match the pattern.
+            for matchedTransaction in matches:
+                # We collect sets of names for each encountered file extension.
+                nameDict = dict()
+                extensions = set()
+                for transactionElem in sorted(matchedTransaction[1:]):
+                    if not (transactionElem.startswith("/") or
+                            transactionElem.startswith("~")):
+                        continue
+
+
+                    # Get the base name and file extension.
+                    ftype = mimetypes.guess_type(transactionElem)[0]
+                    fname = File.getFileNameFromPath(transactionElem)
+                    fnoext = File.getNameWithoutExtensionFromPath(fname)
+                    fext = File.getExtensionFromPath(fname, filterInvalid=True)
+
+                    # Remember which exts were found for a name and overall.
+                    if fext:
+                        extensions.add(fext)
+                        extSet = nameDict.get(fnoext) or set()
+                        extSet.add(fext)
+                        nameDict[fnoext] = extSet
+                
+                # Now check which extension combos exist, and how many times
+                # they occur.
+                extPairOccs = dict()
+                for (fname, extSet) in nameDict.items():
+                    fs = frozenset(extSet)
+                    extPairOccs[fs] = (extPairOccs.get(fs) or 0) + 1
+
+                # Compile list of all valid extension combos, and browse them
+                # in reverse order of length as we first want to validate the
+                # largest combinations.
+                combos = list(extPairOccs.keys())
+                combos.sort(key=len, reverse=True)
+
+                # Count patterns which exclusively have one extension tied to
+                # another (i.e. extension never appears on its own).
+                exclusives = dict()
+                nonExclusiveKeys = set()
+                for k in combos:
+                    # All the subsets of the current combo of filetypes are not
+                    # exclusive since they're included in this set.
+                    subcombos = list()
+                    for i in range(1, len(k)):
+                         subcombos.extend([frozenset(x) for x in 
+                                           itertools.combinations(k, i)])
+                    nonExclusiveKeys.update(subcombos)
+
+                    # Also check if any of these subsets is itself in the list,
+                    # if so the current set is not exclusive.
+                    for sub in subcombos:
+                        if sub in extPairOccs:
+                            break
+                    else:
+                        # Remember: subsets of a previous set aren't exclusive.
+                        if k not in nonExclusiveKeys:
+                            exclusives[k] = extPairOccs[k]
+                    
+                # Now add the match's groups of filenames to counters for the
+                # whole pattern. Count both number of cases where the pattern
+                # is found / exclusively found, and the number of times it is
+                # found.
+                for (k, v) in extPairOccs.items():
+                    counter[k] = (counter.get(k) or 0) + v
+                    counterI[k] = (counterI.get(k) or 0) + 1
+                for (k, v) in exclusives.items():
+                    exclusiveCounter[k] = (exclusiveCounter.get(k) or 0) + v
+                    exclusiveCounterI[k] = (exclusiveCounterI.get(k) or 0) + 1
+
+            # Finally, print information on the pattern.
+            _printPattern(p, matches, counter, exclusiveCounter)
+
+             
+
+#            fe type, if that type exists on its own
+            
+#            267 occs, .a .b .c
+#            dict of all names for each extension
+            
+            
+#            .a: 200 alone, 52 with .b, 15 with .b .c
+#            .a name diversity (200 alone, 52 .b, 15 .b.c)
+#            print lists of distinct filenames for each category.
+#            distribution of folder depths overall
+#            distribution of folder depths per ext (is there an ext always in the same folder?)
+            
+        
+        
+        
+        
+            # TODO add all folder elements to find folders that contain more of a file type
+            # TODO       itemsets with two types are "co-frequent types".
+            # TODO       itemsets without types must be searched for mutually exclusive folders (not parents of one another).
+            # TODO then, itemsets with one type + folders are frequent types for those folders.
+            # TODO       itemsets with just one folder are frequent folders.
+            # TODO       itemsets with just one type are frequent types.
+
 
